bc2159239c4336ca64051249ed8c9341285ced1e
streafy <streafyhs@gmail.com>
2024-06-23 14:28:55 +0700

    hw2 done

--- app/src/main/kotlin/com/streafy/androidacademyfundamentals/MovieDetailsActivity.kt
+++ app/src/main/kotlin/com/streafy/androidacademyfundamentals/MovieDetailsActivity.kt
@@ -1,34 +1,21 @@
 package com.streafy.androidacademyfundamentals
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_details)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val textView: TextView = findViewById(R.id.hello_world)
-        textView.setOnClickListener {
-            openMovieDetailsScreen()
-        }
-    }
-
-    private fun openMovieDetailsScreen() {
-        val intent = Intent(this, MovieDetailsActivity::class.java)
-
-        startActivity(intent)
     }
 }
